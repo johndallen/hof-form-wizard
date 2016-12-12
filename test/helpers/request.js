30af2769a2ed8cf516731ee115638a83c9462d78
joefitter <hello@joefitter.com>
2016-12-12 11:06:57 +0000

    Devtask - ES6ify
    * updated to use es6 code
    * installed eslint-config-homeoffice
    * fixed code in line with linter
    * use hof-model

--- test/helpers/request.js
+++ test/helpers/request.js
@@ -1,8 +1,13 @@
-var reqres = require('reqres'),
-    SessionModel = require('../../lib/model');
+'use strict';
 
-module.exports = function (settings) {
-    var req = reqres.req(settings);
-    req.sessionModel = req.sessionModel || new SessionModel({}, { session: req.session, key: 'hof-wizard-test' });
-    return req;
+const reqres = require('reqres');
+const SessionModel = require('../../lib/model');
+
+module.exports = settings => {
+  const req = reqres.req(settings);
+  req.sessionModel = req.sessionModel || new SessionModel({}, {
+    session: req.session,
+    key: 'hof-wizard-test'
+  });
+  return req;
 };
